e94aa18d6e747c8db5de79500ec4c2bd3c04b3eb
JoeHGeoghegan <jhg55555@gmail.com>
2015-10-27 16:50:34 -0400

    Added a main model file to hold and manage arrays and other information required by the application that is not handled in other class files

---
+++ app/src/main/java/capstone/thermajust/Model/Main_Model.java
@@ -0,0 +1,19 @@
+package capstone.thermajust.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Joe Geoghegan on 10/27/2015.
+ *
+ * The Main_Model class is created to be used in conjunction with the rest of the Thermajust code files.
+ *
+ * The main purpose of this class is to hold an array for each type of object.
+ *  Along with that it also handles their arrangements and operations that involve
+ *      every object in an array.
+ */
+public class Main_Model {
+    ArrayList<Device> deviceList = new ArrayList<Device>();
+    ArrayList<Group> groupList = new ArrayList<Group>();
+//    ArrayList<Schedule> scheduleList = new ArrayList<Schedule>();
+//    ArrayList<Power> powerList = new ArrayList<Power>();
+}
